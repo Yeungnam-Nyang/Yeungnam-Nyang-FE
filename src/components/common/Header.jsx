b639506f4042495921050899519d09946959fd5c
tkv00 <tkv0098@gmail.com>
2024-09-08 20:40:48 +0900

    ✨feat:상단바 및 title 구현

---
+++ src/components/common/Header.jsx
@@ -0,0 +1,26 @@
+import { Link } from "react-router-dom";
+import { FaPen, FaUserPlus } from "react-icons/fa";
+import logo from "../../assets/images/logo.png";
+export default function Header() {
+  return (
+    <nav className="w-full h-20 shadow-global flex gap-4 justify-between align-middle items-center px-3">
+      {/* 홈으로 이동 */}
+      <Link to="/" className="">
+        <img src={logo} alt="logo" className="h-16 w-auto" />
+      </Link>
+      <div className="flex flex-row gap-4 ">
+        <Link to="/post/write" className="hover:scale-125 duration-500">
+          <FaPen size={25} color="#000000" />
+        </Link>
+        {/* 친구 추가 팝업창 */}
+        <button>
+          <FaUserPlus
+            size={25}
+            color="#000000"
+            className="hover:scale-125 duration-500"
+          />
+        </button>
+      </div>
+    </nav>
+  );
+}
