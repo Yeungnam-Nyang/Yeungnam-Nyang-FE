673195e4465f0a5a26d4943eed749eb03429ba08
tkv00 <tkv0098@gmail.com>
2024-09-08 21:54:05 +0900

    ✨feat:공통 버튼 스타일링 구현

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,3 +1,4 @@
+import Button from "../components/common/Button";
 import Header from "../components/common/Header";
 import NavBar from "../components/common/NavBar";
 import Title from "../components/common/title";
@@ -7,6 +8,7 @@ export default function Main() {
     <>
       <Header />
       <Title text={"NEW"} />
+      <Button type={"submit"} text={"ID찾기"} isValid={true} />
       <NavBar />
     </>
   );
