140c3faf16c31fbfb7346c74a2a6b5c913a2980f
tkv00 <tkv0098@gmail.com>
2024-09-11 22:06:48 +0900

    ✨feat:main페이지 ui구현

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -2,13 +2,20 @@ import Button from "../components/common/Button";
 import Header from "../components/common/Header";
 import NavBar from "../components/common/NavBar";
 import Title from "../components/common/Title";
+import PostPreview from "../components/main/PostPreview";
 
 export default function Main() {
   return (
     <>
       <Header />
-      <Title text={"NEW"} />
-      <Button type={"submit"} text={"ID찾기"} isValid={true} />
+      <div className="h-20" />
+      <Title text={"NEW"} padding={28} />
+      {/* <Button type={"submit"} text={"ID찾기"} isValid={true} /> */}
+      <PostPreview />
+      <hr className="bg-white h-1 w-[95%] mx-auto my-5" />
+      <Title text={"HOT"} select="fire" />
+      <PostPreview />
+      <div className="h-20" />
       <NavBar />
     </>
   );
