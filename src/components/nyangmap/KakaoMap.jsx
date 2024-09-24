e52cd2f6f1b7d5689b5b101ac6bc14a7778bb243
tkv00 <tkv0098@gmail.com>
2024-09-24 20:26:24 +0900

    ✨feat:위치 전송송socket 및 위치별 게시글 받아오는 socket구현

--- src/components/nyangmap/KakaoMap.jsx
+++ src/components/nyangmap/KakaoMap.jsx
@@ -5,20 +5,62 @@ import {
   MapMarker,
 } from "react-kakao-maps-sdk";
 import { useGeoLocation } from "../../hooks/useGeoLocation";
-import { useEffect } from "react";
+import { useCallback, useEffect, useRef, useState } from "react";
 import Error from "../common/Error";
 import Marker from "../../assets/images/marker.png";
+import { io } from "socket.io-client";
 export default function KakoMap() {
   //현재 위치 가져오기
   const { location, error, getLocation } = useGeoLocation();
+  //socket
+  const [socket, setSocket] = useState(null);
+  //불러온 고양이 게시물
+  const [catPosts, setCatPosts] = useState([]);
+
+  //현재 위치 소켓으로 서버 전송
+  const sendLocationToServer = useCallback(
+    (lat, lng) => {
+      if (socket) {
+        socket.emit("updateLocation", { latitude: lat, longitude: lng });
+      }
+    },
+    [socket]
+  );
+
+  //마운트 시 소켓 연결
   useEffect(() => {
-    getLocation();
+    const newSocket = io("http://backend");
+    setSocket(newSocket);
+
+    newSocket.on("nearCatPosts", (post) => {
+      setCatPosts(post);
+    });
+
+    //컴포넌트 언마운트 시 소켓 해제
+    return () => newSocket.disconnect();
   }, []);
+
+  useEffect(() => {
+    const updateLocation = async () => {
+      await getLocation();
+      if (location) {
+        sendLocationToServer(location.latitude, location.longitude);
+      }
+    };
+    updateLocation();
+    const intervalId = setInterval(updateLocation, 3000);
+
+    return () => clearInterval(intervalId);
+  }, [getLocation, location, sendLocationToServer]);
+
   if (error) {
     return <Error />;
   }
 
   const defaultLocation = { lat: 35.8264595, lng: 128.754132 };
+
+  //   TODO
+  // 소켓을 통해서 실시간 위치 계속 받아오기
   return (
     <div className="py-10">
       <Map
@@ -45,6 +87,12 @@ export default function KakoMap() {
             }}
           ></MapMarker>
         )}
+        {catPosts.map((post, idx) => (
+          <MapMarker
+            key={idx}
+            position={{ lat: post.latitude, lng: post.longitude }}
+          ></MapMarker>
+        ))}
       </Map>
     </div>
   );
