910146e07fecc55501ca72aad5b957ee52c89a1d
tkv00 <tkv0098@gmail.com>
2024-11-24 02:16:08 +0900

    ✨feat:Provider Route 설정

--- src/App.jsx
+++ src/App.jsx
@@ -17,6 +17,8 @@ import DetailPost from "./pages/Post/DetailPost";
 import React from "react";
 import SignUpTest from "./test/signup-test";
 import Friend from "./pages/Friend";
+import PrivateRoutes from "./utils/PrivateRoutes";
+import { AuthProvider } from "./store/AuthProvider";
 class ErrorBoundary extends React.Component {
   constructor(props) {
     super(props);
@@ -44,28 +46,31 @@ class ErrorBoundary extends React.Component {
 }
 function App() {
   return (
-    <ErrorBoundary>
-      <div className="max-w-[768px] min-h-screen mx-auto my-0  bg-yellow shadow-global ">
-        <Routes>
-          <Route path="/" element={<Main />}></Route>
-          <Route path="/signup1" element={<SignUp1 />}></Route>
-          <Route path="/signup2" element={<SignUp2 />}></Route>
-          <Route path="/login" element={<Login />}></Route>
-          <Route path="/login/findid" element={<FindId />}></Route>
-          <Route path="/login/findpwd" element={<FindPassword />}></Route>
-          <Route path="/catmap" element={<CatsMap />}></Route>
-          <Route path="/profile" element={<MyProfile />}></Route>
-          <Route path="/profile/edit" element={<EditProfile />}></Route>
-          <Route path="/profile/mycat" element={<MyCat />}></Route>
-          <Route path="/profile/posts" element={<SavedPosts />}></Route>
-          <Route path="/post/write" element={<WritePost />}></Route>
-          <Route path="/post/:id" element={<DetailPost />}></Route>
-          <Route path="/friend" element={<Friend />}></Route>
-          <Route path="/*" element={<NotFound />}></Route>
-          <Route path="/api/signuptest" element={<SignUpTest />}></Route>
-        </Routes>
-      </div>
-    </ErrorBoundary>
+    <AuthProvider>
+      <ErrorBoundary>
+        <div className="max-w-[768px] min-h-screen mx-auto my-0  bg-yellow shadow-global ">
+          <Routes>
+            <Route path="/signup1" element={<SignUp1 />}></Route>
+            <Route path="/signup2" element={<SignUp2 />}></Route>
+            <Route path="/login" element={<Login />}></Route>
+            <Route path="/login/findid" element={<FindId />}></Route>
+            <Route path="/login/findpwd" element={<FindPassword />}></Route>
+            <Route element={<PrivateRoutes />}>
+              <Route path="/" element={<Main />}></Route>
+              <Route path="/catmap" element={<CatsMap />}></Route>
+              <Route path="/profile" element={<MyProfile />}></Route>
+              <Route path="/profile/edit" element={<EditProfile />}></Route>
+              <Route path="/profile/mycat" element={<MyCat />}></Route>
+              <Route path="/profile/posts" element={<SavedPosts />}></Route>
+              <Route path="/post/write" element={<WritePost />}></Route>
+              <Route path="/post/:id" element={<DetailPost />}></Route>
+              <Route path="/friend" element={<Friend />}></Route>
+              <Route path="/*" element={<NotFound />}></Route>
+            </Route>
+          </Routes>
+        </div>
+      </ErrorBoundary>
+    </AuthProvider>
   );
 }
 
