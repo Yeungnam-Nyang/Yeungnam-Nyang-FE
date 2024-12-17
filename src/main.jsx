7d177853c81b4c0ff7a80ba2150ed55e9ee356fb
tkv00 <tkv0098@gmail.com>
2024-12-17 19:43:34 +0900

    🐛fix:카카오맵 오류 잠정적 해결

--- src/main.jsx
+++ src/main.jsx
@@ -3,13 +3,12 @@ import App from "./App.jsx";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "react-query";
-import { ReactQueryDevtools } from "react-query/devtools";
+
 const queryClient = new QueryClient();
 createRoot(document.getElementById("root")).render(
   <QueryClientProvider client={queryClient}>
     <BrowserRouter>
       <App />
-      <ReactQueryDevtools />
     </BrowserRouter>
   </QueryClientProvider>
 
